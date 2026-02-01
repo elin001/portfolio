faf868745e0e5f85724ac629893d4e12a0909a81
Ellie Lin <ellie.ellielin@gmail.com>
2026-01-31 18:08:53 -0600

    Create Hed.svelte

---
+++ src/components/Hed.svelte
@@ -0,0 +1,13 @@
+<script>
+  let { offTheClock, isMobile } = $props();
+</script>
+
+{#if isMobile}
+  <h1>Ellie Lin</h1>
+{:else}
+  <h1>
+    Hi, I’m <span class="emphasis">Ellie Lin,</span>
+    <br />visual journalist
+  </h1>
+{/if}
+ 

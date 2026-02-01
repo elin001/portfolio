1d6f00e09b97c69c70197469b738405f4c97a81a
Ellie Lin <ellie.ellielin@gmail.com>
2026-01-31 18:08:39 -0600

    Create App1.svelte

---
+++ src/App1.svelte
@@ -0,0 +1,21 @@
+<script>
+  import data from "../data/portfolio.json";
+
+  import Hed from "./components/Hed.svelte";
+
+  let innerWidth = $state(0);
+  let isMobile = $derived(innerWidth < 768);
+  let isTablet = $derived(innerWidth >= 768 && innerWidth < 1024);
+  let isDesktop = $derived(innerWidth >= 1024);
+
+  let offTheClock = $state(false);
+
+  $inspect(data);
+</script>
+
+<svelte:window bind:innerWidth />
+
+<Hed {offTheClock} {isMobile} />
+
+<style>
+</style>

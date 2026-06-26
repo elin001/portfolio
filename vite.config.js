0234d783467fc60ce1c28c5effecb42df82ce592
Ellie Lin <ellie.ellielin@gmail.com>
2026-06-26 14:27:44 -0500

    fixes

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [tailwindcss(), svelte()],
+  plugins: [svelte(), tailwindcss()],
 })
